43a4e08bc8d8f0fbc7641c81c9b009b6b46031cb
lzmhhh123 <lzmhhh123@gmail.com>
2017-11-28 14:56:26 +0800

    complete homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,23 +1,43 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, Image, Text} from 'react-native';
-import {Card, Icon} from 'react-native-elements';
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  Linking
+} from 'react-native';
+import {Card, Icon, Button} from 'react-native-elements';
 
 export default class extends Component {
+  constructor() {
+    super()
+  }
   static navigationOptions = {
     tabBarLabel: 'Home',
     // Note: By default the icon is only shown on iOS. Search the showIcon option below.
     tabBarIcon: <Icon name="home" />,
   };
   render() {
+    let url = "https://github.com/lzmhhh123/LLphoto";
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Card
-          image={require("../img/LLphoto.jpg")}
-          title="ABOUT APP"
-        >
+        <Card title="ABOUT APP">
+          <Image source={require("../img/LOGO2.png")} resizeMode="contain" style={{width: '100%'}}/>
           <Text style={{marginBottom: 10}} >
-            A photo ablum converting your videos into structural and searchable metadata.
+            A photo ablum converting your photos into structural and searchable metadata.
           </Text>
+          <Button
+            icon={{name: 'home'}}
+            backgroundColor='black'
+            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+            title='Follow us on Github'
+            onPress={() => Linking.canOpenURL(url).then(supported => {
+              if (supported) {
+                Linking.openURL(url);
+              } else {
+                console.log('Can not open ' + url);
+              }
+            })} />
         </Card>
       </View>
     )
